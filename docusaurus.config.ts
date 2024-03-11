4403081c2641da72092a86673f81853aaaafdbb2
Santthosh Selvadurai <santthosh@users.noreply.github.com>
2024-03-11 15:29:09 -0700

    Adding consolidated docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -97,23 +97,6 @@ const config: Config = {
           ],
         },
         {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
           title: 'More',
           items: [
             {
@@ -122,12 +105,12 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/assistants-hub/assistantshub.ai',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Assistants Hub, Inc. All rights reserved.`,
     },
     prism: {
       theme: prismThemes.github,
